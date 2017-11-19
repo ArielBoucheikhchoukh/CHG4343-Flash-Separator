a16c555791282954d907a669bc9d70d1d42cdd52
ArielBoucheikhchoukh <32803387+ArielBoucheikhchoukh@users.noreply.github.com>
2017-11-19 01:29:37 -0500

    Delete OutOfFunctionBoundsException.java

--- OutOfFunctionBoundsException.java
+++
@@ -1,5 +0,0 @@
-public class OutOfFunctionBoundsException extends FunctionException {
-   public OutOfFunctionBoundsException() {
-     super("OutOfFunctionBoundsException: The independent variable does not fall within the bounds of the function."); 
-   }
- }
