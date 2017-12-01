725f708c8a941cabfd548eaa7b1857dd6b279691
ArielBoucheikhchoukh <32803387+ArielBoucheikhchoukh@users.noreply.github.com>
2017-12-01 02:38:46 -0500

    Delete UndefinedIndependentVariableException.java

--- UndefinedIndependentVariableException.java
+++
@@ -1,9 +0,0 @@
-
-public class UndefinedIndependentVariableException extends FunctionException {
-	
-	public UndefinedIndependentVariableException(String functionID) {
-		super("UndefinedIndependentVariableException: Function was evaluated at an undefined value.", 
-				functionID);
-	}
-	
-}
