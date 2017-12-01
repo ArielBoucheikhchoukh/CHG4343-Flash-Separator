f1d05582ec1e19902c5ac84090c28f0854bb0b34
ArielBoucheikhchoukh <32803387+ArielBoucheikhchoukh@users.noreply.github.com>
2017-12-01 02:39:29 -0500

    Delete FlashNotPossibleException.java

--- FlashNotPossibleException.java
+++
@@ -1,5 +0,0 @@
-public class FlashNotPossibleException extends FlashCalculationException {
-	public FlashNotPossibleException() {
-		super("FlashNotPossibleException: Flash separation of this stream is not possible.");
-	}
-}
