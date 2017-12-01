4470f1265a8882d35cded4ec8abb26380d8683de
ArielBoucheikhchoukh <32803387+ArielBoucheikhchoukh@users.noreply.github.com>
2017-12-01 02:40:13 -0500

    Delete NoRootWithinFunctionBoundsException.java

--- NoRootWithinFunctionBoundsException.java
+++
@@ -1,5 +0,0 @@
-public class NoRootWithinFunctionBoundsException extends NumericalMethodException {
-  public NoRootWithinFunctionBoundsException() {
-   super("NoRootWithinFunctionBoundsException: No root exists within the bounds of the function."); 
-  }
-}
